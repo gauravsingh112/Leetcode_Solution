614e5ef010becfd347de9a902f88a4aa7dc36e4b
gauravsingh112 <129499661+gauravsingh112@users.noreply.github.com>
2023-04-22 23:06:29 +0530

    Added solution - LeetHub

--- Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
+++ Union of Two Sorted Arrays - GFG/union-of-two-sorted-arrays.cpp
@@ -13,36 +13,19 @@ class Solution{
     {
         //Your code here
         //return vector with correct order of elements
-        int i = 0, j = 0; // pointers
-  vector < int > Union; // Uninon vector
-  while (i < n && j < m) {
-    if (arr1[i] <= arr2[j]) // Case 1 and 2
-    {
-      if (Union.size() == 0 || Union.back() != arr1[i])
-        Union.push_back(arr1[i]);
-      i++;
-    } else // case 3
-    {
-      if (Union.size() == 0 || Union.back() != arr2[j])
-        Union.push_back(arr2[j]);
-      j++;
+        set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(arr1[i]);
+        }
+         for(int i=0;i<m;i++){
+            st.insert(arr2[i]);
+        }
+        vector<int>temp;
+        for(auto it:st){
+            temp.push_back(it);
+            
+        }return temp;
     }
-  }
-  while (i < n) // IF any element left in arr1
-  {
-    if (Union.back() != arr1[i])
-      Union.push_back(arr1[i]);
-    i++;
-  }
-  while (j < m) // If any elements left in arr2
-  {
-    if (Union.back() != arr2[j])
-      Union.push_back(arr2[j]);
-    j++;
-  }
-  return Union;
-}
-    
 };
 
 //{ Driver Code Starts.
